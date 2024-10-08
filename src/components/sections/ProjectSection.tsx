f24dd08b929aecac9a0febeb28041a1fbfc9b9d2
tyronejose <alt.tyronejose@gmail.com>
2024-10-08 14:03:41 -0300

    feat: add animations for skill section

--- src/components/sections/ProjectSection.tsx
+++ src/components/sections/ProjectSection.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 // import Image from "next/image";
 import Link from "next/link";
 import { Button, Chip } from "@nextui-org/react";
@@ -14,65 +16,67 @@ export const ProjectSection = ({ projects }: Props) => {
   const t = useTranslations("ProjectSection");
 
   return (
-    <section className="space-y-4">
+    <section>
       <Header title="Projects" subtitle="Lorem ipsum" headingLevel="h2" />
-      {projects.map((project) => (
-        <article
-          key={project.id}
-          className="group grid grid-cols-1 sm:grid-cols-2 gap-4 bg-neutral-lightgray dark:bg-neutral-darkgrey border border-neutral-midlight dark:border-neutral-middark hover:border-neutral-gray dark:hover:border-neutral-gray hover:transition-all duration-300 p-4 rounded-xl"
-        >
-          <div className="inline-flex flex-col">
-            <div className="border-b border-b-neutral-midlight dark:border-b-neutral-middark group-hover:border-b-neutral-gray dark:group-hover:border-b-neutral-gray hover:transition-all duration-300 pb-2">
-              <h3 className="scroll-m-20 text-2xl font-bold tracking-tight line-clamp-1">
-                {project.name}
-              </h3>
-            </div>
-            <p className="flex items-center text-sm text-primary text-bold space-x-2 pt-2">
-              <Folder size={16} />
-              <span>Project</span>
-            </p>
-            <div className="space-y-4 py-4">
-              <p className="text-sm text-neutral-500 line-clamp-3">
-                {t(project.descriptionKey)}
+      <div className="space-y-4">
+        {projects.map((project) => (
+          <article
+            key={project.id}
+            className="group grid grid-cols-1 sm:grid-cols-2 gap-4 bg-neutral-lightgray dark:bg-neutral-darkgrey border border-neutral-midlight dark:border-neutral-middark hover:border-neutral-gray dark:hover:border-neutral-gray hover:transition-all duration-300 p-4 rounded-xl"
+          >
+            <div className="inline-flex flex-col">
+              <div className="border-b border-b-neutral-midlight dark:border-b-neutral-middark group-hover:border-b-neutral-gray dark:group-hover:border-b-neutral-gray hover:transition-all duration-300 pb-2">
+                <h3 className="scroll-m-20 text-2xl font-bold tracking-tight line-clamp-1">
+                  {project.name}
+                </h3>
+              </div>
+              <p className="flex items-center text-sm text-primary text-bold space-x-2 pt-2">
+                <Folder size={16} />
+                <span>Project</span>
               </p>
-              <div className="flex space-x-2">
-                {project.skills.map((skill, index) => (
-                  <Chip
-                    key={index}
-                    size="sm"
-                    color="primary"
-                    variant="bordered"
-                    className="text-xs font-bold"
-                  >
-                    {skill}
-                  </Chip>
-                ))}
+              <div className="space-y-4 py-4">
+                <p className="text-sm text-neutral-500 line-clamp-3">
+                  {t(project.descriptionKey)}
+                </p>
+                <div className="flex space-x-2">
+                  {project.skills.map((skill, index) => (
+                    <Chip
+                      key={index}
+                      size="sm"
+                      color="primary"
+                      variant="bordered"
+                      className="text-xs font-bold"
+                    >
+                      {skill}
+                    </Chip>
+                  ))}
+                </div>
+              </div>
+              <div className="flex space-x-2 mt-auto">
+                <Button color="primary" className="text-sm font-medium">{t("linkBtn")}</Button>
+                <Link
+                  href="https://lucide.dev/icons/chevrons-down"
+                  className="p-2 hover:bg-neutral-lightgray dark:hover:bg-neutral-darkgrey dark:hover:text-primary items-center rounded-xl"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Gmail"
+                >
+                  <GitHubIcon />
+                </Link>
               </div>
             </div>
-            <div className="flex space-x-2 mt-auto">
-              <Button color="primary" className="text-sm font-medium">{t("linkBtn")}</Button>
-              <Link
-                href="https://lucide.dev/icons/chevrons-down"
-                className="p-2 hover:bg-neutral-lightgray dark:hover:bg-neutral-darkgrey dark:hover:text-primary items-center rounded-xl"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Gmail"
-              >
-                <GitHubIcon />
-              </Link>
-            </div>
-          </div>
-          <figure className="rounded-xl overflow-hidden border dark:border-neutral-middark aspect-video">
-            {/* <Image
+            <figure className="rounded-xl overflow-hidden border dark:border-neutral-middark aspect-video">
+              {/* <Image
               src={project.image}
               alt={project.name}
               width={300}
               height={200}
               className="group-hover:scale-110 transition-all ease-in-out"
             /> */}
-          </figure>
-        </article>
-      ))}
+            </figure>
+          </article>
+        ))}
+      </div>
     </section>
   )
 }
--- src/components/sections/SkillSection.tsx
+++ src/components/sections/SkillSection.tsx
@@ -1,33 +1,50 @@
+"use client"
+
 import { Skill } from "@/interfaces";
 import Image from "next/image";
 import { Header } from "@/components";
 import { Tooltip } from "@nextui-org/react";
+import { useAnimateOnView } from "@/hooks";
+import { motion } from 'framer-motion';
 
 interface Props {
   skills: Skill[];
 }
 
 export const SkillSection = ({ skills }: Props) => {
+  const { ref, controls, itemVariants } = useAnimateOnView(0.1, false);
+
   return (
     <section className="space-y-4">
       <Header title="Skills" subtitle="Lorem ipsum" headingLevel="h2" />
-      <div className="grid grid-cols-4 sm:grid-cols-6 lg:grid-cols-8 gap-2">
-        {skills.map((skill) => (
-          <Tooltip
+      <div className="grid grid-cols-4 sm:grid-cols-6 lg:grid-cols-8 gap-2" ref={ref}>
+        {skills.map((skill, idx) => (
+          <motion.div
             key={skill.id}
-            offset={15}
-            color="primary"
-            placement="top"
-            content={skill.name}
+            variants={itemVariants}
+            initial="hidden"
+            animate={controls}
+            transition={{
+              duration: 0.3,
+              delay: idx * 0.1,
+              ease: "easeOut",
+            }}
           >
-            <Image
-              src={skill.icon}
-              alt={skill.name}
-              width={200}
-              height={200}
-              className="bg-neutral-lightgray dark:bg-neutral-darkgrey border border-neutral-midlight dark:border-neutral-middark hover:border-neutral-gray dark:hover:border-neutral-gray hover:transition-all duration-300 p-4 rounded-xl"
-            />
-          </Tooltip>
+            <Tooltip
+              offset={15}
+              color="primary"
+              placement="top"
+              content={skill.name}
+            >
+              <Image
+                src={skill.icon}
+                alt={skill.name}
+                width={200}
+                height={200}
+                className="bg-neutral-lightgray dark:bg-neutral-darkgrey border border-neutral-midlight dark:border-neutral-middark hover:border-neutral-gray dark:hover:border-neutral-gray hover:transition-all duration-300 p-4 rounded-xl"
+              />
+            </Tooltip>
+          </motion.div>
         ))}
       </div>
     </section>
